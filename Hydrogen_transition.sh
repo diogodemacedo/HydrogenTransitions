33454ee9acc5108750f485cde48291dcbf17d5c1
Diogo de Macedo <104172784+diogodemacedo@users.noreply.github.com>
2023-02-17 12:20:26 +0100

    Add files via upload

---
+++ Hydrogen_transition.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#Diogo de Macedo
+
+# User input
+
+echo "Enter the initial energy state (n1) of the hydrogen atom:"
+read n1
+echo "Enter the final energy state (n2) of the hydrogen atom:"
+read n2
+
+# Constants
+
+h=6.626e-34  # Planck's constant in joule-seconds
+c=299792458       # speed of light in meters per second
+m=9.109e-31  # mass of an electron in kilograms
+e=1.602e-19  # elementary charge in coulombs
+R=1.098e7 # Rydberg constant in inverse meters
+
+# Calculate the transition energy in joules
+energy=$(awk "BEGIN {print $h*$c*$R*(1/$n1/$n1-1/$n2/$n2)}")
+
+# Calculate the energy in electronvolts
+energy_eV=$(awk "BEGIN {print $energy/$e}")
+
+# Calculate the wavelength of the emitted light in meters
+wavelength=$(awk "BEGIN {print $h*$c/$energy}")
+
+# Display the results
+echo "The energy of the transition is $energy_eV eV." 
+echo "The energy of the emitted photon is $energy J." 
+echo "The wavelength of the emitted light is $wavelength m."
+
